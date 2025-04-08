3f7961919f23b3cb10fb6719cbfe1d9627891c1c
Lautaro Rodriguez <taty_0321@hotmail.com>
2025-04-08 13:25:22 -0300

    Week 05 HW

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment hw1 = new Assignment("John", "Math");
+        Console.WriteLine(hw1.GetSummary());
+
+        MathAssignment hw2 = new MathAssignment("Roberto Rodriguez","Fractions", "7.3", "8-19");
+        Console.WriteLine(hw2.GetSummary());
+        Console.WriteLine(hw2.GetHomeworkList());
+
+        WritingAssignment hw3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II" );
+        Console.WriteLine(hw3.GetSummary());
+        Console.WriteLine(hw3.GetWritingInformation());
     }
 }
