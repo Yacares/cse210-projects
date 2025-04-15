cba6eca85774f93b5ed52a999d2e388c97d534c0
Lautaro Rodriguez <taty_0321@hotmail.com>
2025-04-15 14:47:02 -0300

    HW week06 Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+
+         List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Green", 2.0);
+        Rectangle r1 = new Rectangle("Red", 4.0, 5.0);
+        Circle c1 = new Circle("Blue", 3.0);
+
+        shapes.Add(s1);
+        shapes.Add(r1);
+        shapes.Add(c1);
+
+        foreach (Shape shape in shapes)
+        {
+            DisplayInformation(shape);
+        }
+    }
+
+    public static void DisplayInformation(Shape shape)
+    {
+        Console.WriteLine($"This is a {shape.GetColor()} {shape.GetType().Name} with an area of {shape.GetArea()}");
     }
 }
